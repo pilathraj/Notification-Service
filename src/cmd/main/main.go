0bd5c9289764a560826a2027cab51d86f0cadde1
pilathraj <pilathraj@gmail.com>
2025-05-10 17:15:36 +0530

    Add files via upload

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -5,47 +5,116 @@ import (
 	"fmt"
 	"log"
 
-	consumer_service "notification-service/cmd/consumer"
-	producer_service "notification-service/cmd/producer"
-	"notification-service/pkg/models"
+	notifications "notification-service/src/cmd/services/notifications"
+	notifychannels "notification-service/src/cmd/services/notify-channels"
+	preferences "notification-service/src/cmd/services/preferences"
+	consumer_service "notification-service/src/consumer"
+
+	"notification-service/src/pkg/models"
+
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const (
-	LaunchPort = ":8081"
+	LaunchPort = ":8083"
 )
+
+var KafkaBroker = os.Getenv("KAFKA_BROKERS")
+var KafkaTopic = os.Getenv("KAFKA_TOPIC")
+
+var db *gorm.DB
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 
-	// Sample users for testing
-	users := []models.User{
-		{ID: 1, Name: "Pilathraj"},
-		{ID: 2, Name: "Prakash"},
-		{ID: 3, Name: "Gopika"},
-		{ID: 4, Name: "Manasa"},
-		{ID: 5, Name: "Himanshu"},
+	var err error
+	dsn := os.Getenv("POSTGRES_DATABASE_DSN")
+	//dsn := "host=postgres-service user=root password=postgres dbname=notifications_db port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	if dsn == "" {
+		log.Fatalf("Missing POSTGRES_DATABASE_DSN environment variable")
 	}
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	// Auto-migrate the Notification and Preference models
+	if err := db.AutoMigrate(&models.Notification{}, &models.UserPreferences{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	// Initialize Kafka producer
+	producer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{KafkaBroker},
+		Topic:   KafkaTopic,
+	})
+	defer producer.Close()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	store := &consumer_service.NotificationStore{
 		Data: make(consumer_service.UserNotifications),
 	}
-
-	producer, err := producer_service.SetupProducer()
-	if err != nil {
-		log.Fatalf("failed to initialize producer: %v", err)
-	}
-	defer producer.Close()
-
-	ctx, cancel := context.WithCancel(context.Background())
 	go consumer_service.SetupConsumerGroup(ctx, store)
 	defer cancel()
 
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.Default()
-	router.POST("/send", producer_service.SendMessageHandler(producer, users))
-	router.GET("/notifications/:userID", func(ctx *gin.Context) {
+	// Notification Management
+	router.POST("/api/notifications", func(ctx *gin.Context) {
+		notifications.CreateNotification(ctx, producer, db)
+	})
+	router.GET("/api/notifications/user/:userId", func(ctx *gin.Context) {
+		notifications.GetUserNotifications(ctx, db)
+	})
+	router.PUT("/api/notifications/:notificationId/read", func(ctx *gin.Context) {
+
+		notifications.MarkNotificationAsRead(ctx, db)
+	})
+	router.DELETE("/api/notifications/:notificationId", func(ctx *gin.Context) {
+		notifications.DeleteNotification(ctx, db)
+	})
+
+	// Preferences Management
+	router.GET("/api/notifications/preferences/:userId", func(ctx *gin.Context) {
+		preferences.GetUserPreferences(ctx, db)
+	})
+	router.PUT("/api/notifications/preferences/:userId", func(ctx *gin.Context) {
+		preferences.UpdateUserPreferences(ctx, db)
+	})
+
+	router.GET("/api/notifications/notify", func(ctx *gin.Context) {
+		notifychannels.SetupConsumerGroup(ctx)
+	})
+
+	router.GET("/api/notifications/consume/:userID", func(ctx *gin.Context) {
 		consumer_service.HandleNotifications(ctx, store)
+	})
+
+	router.GET("/api/notifications/consume", func(ctx *gin.Context) {
+		consumer_service.HandleAllNotifications(ctx, store)
 	})
 
 	fmt.Printf("Nofification service 📨 started at http://localhost%s\n",
